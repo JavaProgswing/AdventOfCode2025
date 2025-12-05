802e5b2fb8010b94c7ceba74473f49e863afa900
TJP <69581225+JavaProgswing@users.noreply.github.com>
2025-12-05 11:00:16 +0530

    refactor: Added Solution0 for convenience.

---
+++ src/Solution0.java
@@ -0,0 +1,27 @@
+public class Solution0 extends Solution {
+    public Solution0() {
+        super(0);
+    }
+
+    public static void main(String[] args) {
+        Solution0 solution = new Solution0();
+        solution.testPartOneSample();
+    }
+
+
+    @Override
+    String solvePartOne(String inputData) {
+        for (String line : inputData.split("\n")) {
+
+        }
+        return null;
+    }
+
+    @Override
+    String solvePartTwo(String inputData) {
+        for (String line : inputData.split("\n")) {
+
+        }
+        return null;
+    }
+}
